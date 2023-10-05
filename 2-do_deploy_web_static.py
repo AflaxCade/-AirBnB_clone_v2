4e34e9006bb0ea11aadd092312b0f34ab6e17854
AflaxCade <116007622+AflaxCade@users.noreply.github.com>
2023-10-05 16:41:04 +0300

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,19 +1,43 @@
 #!/usr/bin/python3
-"""Compress web static package
 """
-from fabric.api import *
-from datetime import datetime
-from os import path
+Fabric script that distributes an archive to your web servers
+"""
+
+from fabric import task
+from fabric import Connection
+import os
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.key_filename = '~/.ssh/school'
+env.hosts = ['34.224.16.126', '54.237.34.32']
 
 
-def do_deploy(archive_path):
-        """Deploy web files to server
+@task
+def do_deploy(c, archive_path):
         """
-        try:
-                if not (path.exists(archive_path)):
-                        return False
+        return the archive path if archive has generated correctly.
+    """
+        
+    if not os.path.exists(archive_path):
+        return False
+
+    archive_filename = os.path.basename(archive_path)
+    archive_folder = "/data/web_static/releases/{}".format(
+        archive_filename.split(".")[0]
+    )
+
+    try:
+        c.put(archive_path, "/tmp/")
+        c.run("mkdir -p {}".format(archive_folder))
+        c.run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
+        c.run("rm /tmp/{}".format(archive_filename))
+        c.run("mv {}/web_static/* {}/".format(archive_folder, archive_folder))
+        c.run("rm -rf {}/web_static".format(archive_folder))
+        c.run("rm -rf /data/web_static/current")
+        c.run("ln -s {} /data/web_static/current".format(archive_folder))
+
+        return True
+
+    except Exception:
+        return False
